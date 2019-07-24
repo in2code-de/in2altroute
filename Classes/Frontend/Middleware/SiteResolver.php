021918a759caca52023e32fd1663e95b438cae85
Oliver Eglseder <oliver.eglseder@in2code.de>
2019-07-24 18:11:02 +0200

    [BUGFIX] Do not try to resolve empty paths by page slugs

--- Classes/Frontend/Middleware/SiteResolver.php
+++ Classes/Frontend/Middleware/SiteResolver.php
@@ -129,42 +129,44 @@ class SiteResolver implements MiddlewareInterface
         if (!($language instanceof SiteLanguage)) {
             // FIXME: START
             $path = $request->getUri()->getPath();
-            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
-            $queryBuilder->getRestrictions()->removeAll()->add(new FrontendRestrictionContainer());
-            $queryBuilder->select('*')
-                         ->from('pages')
-                         ->where($queryBuilder->expr()->eq('slug', $queryBuilder->createNamedParameter($path)));
-            $pagesStatement = $queryBuilder->execute();
-            $sites = [];
-            while ($row = $pagesStatement->fetch()) {
-                $site = $this->siteFinder->getSiteByPageId($row['uid']);
-                $sites[$site->getIdentifier()] = $site;
-            }
-            $collection = $this->getRouteCollectionForSites($sites);
-            $context = new RequestContext(
-                '',
-                $request->getMethod(),
-                HttpUtility::idn_to_ascii($request->getUri()->getHost()),
-                $request->getUri()->getScheme(),
-                // Ports are only necessary for URL generation in Symfony which is not used by TYPO3
-                80,
-                443,
-                $request->getUri()->getPath()
-            );
-            $matcher = new UrlMatcher($collection, $context);
-            try {
-                $result = $matcher->match($request->getUri()->getPath());
-                return new SiteRouteResult(
-                    $request->getUri(),
-                    $result['site'],
-                    // if no language is found, this usually results due to "/" called instead of "/fr/"
-                    // but it could also be the reason that "/index.php?id=23" was called, so the default
-                    // language is used as a fallback here then.
-                    $result['language'] ?? $defaultLanguage,
-                    $result['tail']
+            if (!empty(trim($path, '/'))) {
+                $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+                $queryBuilder->getRestrictions()->removeAll()->add(new FrontendRestrictionContainer());
+                $queryBuilder->select('*')
+                             ->from('pages')
+                             ->where($queryBuilder->expr()->eq('slug', $queryBuilder->createNamedParameter($path)));
+                $pagesStatement = $queryBuilder->execute();
+                $sites = [];
+                while ($row = $pagesStatement->fetch()) {
+                    $site = $this->siteFinder->getSiteByPageId($row['uid']);
+                    $sites[$site->getIdentifier()] = $site;
+                }
+                $collection = $this->getRouteCollectionForSites($sites);
+                $context = new RequestContext(
+                    '',
+                    $request->getMethod(),
+                    HttpUtility::idn_to_ascii($request->getUri()->getHost()),
+                    $request->getUri()->getScheme(),
+                    // Ports are only necessary for URL generation in Symfony which is not used by TYPO3
+                    80,
+                    443,
+                    $request->getUri()->getPath()
                 );
-            } catch (NoConfigurationException | ResourceNotFoundException $e) {
-                // No site+language combination found so far
+                $matcher = new UrlMatcher($collection, $context);
+                try {
+                    $result = $matcher->match($request->getUri()->getPath());
+                    return new SiteRouteResult(
+                        $request->getUri(),
+                        $result['site'],
+                        // if no language is found, this usually results due to "/" called instead of "/fr/"
+                        // but it could also be the reason that "/index.php?id=23" was called, so the default
+                        // language is used as a fallback here then.
+                        $result['language'] ?? $defaultLanguage,
+                        $result['tail']
+                    );
+                } catch (NoConfigurationException | ResourceNotFoundException $e) {
+                    // No site+language combination found so far
+                }
             }
             // FIXME: END
 
