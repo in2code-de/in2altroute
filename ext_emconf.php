ecda4eb209298711cbd3cd596ce31349f805f3ac
eds19957 <stephan.ederer@ur.de>
2019-11-27 08:11:24 +0100

    undo version change

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'in2altroute-eds',
+    'title' => 'in2altroute',
     'description' => 'TYPO3 extension to support speaking urls without a subdomain or fixed path segment',
     'category' => 'fe',
     'author' => 'Oliver Eglseder',
     'author_email' => 'oliver.eglseder@in2code.de',
     'state' => 'experimental',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.3',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
